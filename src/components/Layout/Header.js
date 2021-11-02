ea5bb014c09886e7b16f764a89b96afe2e8058dc
Basil T T <tt.basil@gmail.com>
2021-11-02 11:20:58 +0530

    Add header component
    Header.js and Header.module.css files added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+function Header(props) {
+    return (
+        <div className={styles.header}>
+            <button></button>
+        </div>
+    );
+}
+
+export default Header;
