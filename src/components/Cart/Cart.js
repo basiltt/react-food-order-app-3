528569f8f37b632d8d55aaaf5d8e303fbd96062c
Basil T T <tt.basil@gmail.com>
2021-11-03 11:30:45 +0530

    Add Cart Component with dummy values
    Create Cart.js and Cart.module.css files

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Cart.module.css'
+
+
+function Cart(props) {
+    const cartItems = (
+        <ul className={styles['cart-items']}>
+            {[{id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) => <li>{item.name}</li>)}
+        </ul>
+    );
+    return (
+        <div>
+            {cartItems}
+            <div className={styles.total}>
+                <span>Total Amount</span>
+                <span>$23.22</span>
+            </div>
+            <div className={styles.actions}>
+                <button>Close</button>
+                <button>Order</button>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;
