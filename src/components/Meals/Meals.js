ecfe5f8b6a644a28c529848c5d7e324dd1743228
Basil T T <tt.basil@gmail.com>
2021-11-03 07:25:44 +0530

    Add available meals section
    Create new files AvailableMeals.js, AvailableMeals.module.css, Card.js, Card.module.css files and modify Meals.js file

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 
 function Meals(props) {
     return (
         <Fragment>
             <MealsSummary />
+            <AvailableMeals />
         </Fragment>
     );
 }
